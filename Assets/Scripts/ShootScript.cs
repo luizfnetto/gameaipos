5efe45b488dffcf05e590fd0d7d170908e6895ac
GiuDuncan <giuliaduncan@gmail.com>
2019-12-15 17:59:37 -0300

    now shooting

---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+
+    }
+    
+}
